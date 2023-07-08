e59ef0ad04a91ea8e16448dc125de32694728a5a
Larry Le <larrygsu@gmail.com>
2023-07-08 00:14:37 -0400

    Updating with completed exercise

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+const solution = document.getElementById("solution");
+
+const madlib = document.getElementById("madlib");
+
+madlib.addEventListener('submit', (e) => {
+  console.log("here")
+  let form = e.target;
+  solve(form.name.value,form.place.value,form.verb.value,form.Place2.value,form.adjective.value,form.creature.value,form.adjective2.value,);
+  e.preventDefault();
+})
+
+function solve(name,place,verb,place2,adjective,creature,adjective2) 
+{
+  solution.innerText = `When we were little, my friend ${name} and I would go to the ${place} and ${verb} at the ${place2} catching ${adjective} ${creature} until we were ${adjective2}.`
+}
